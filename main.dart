97acbe2d7cbb19ef33ddc8f778136282f631c4cc
iamefficieny <80177103+iamefficieny@users.noreply.github.com>
2022-05-28 21:29:02 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+import "dart:io";
+import "dart:math";
+void main() {
+  String name = "Tafadzwa Tandy";
+  String favoriteApp = "Instagram";
+  String city = "Johannesburg";
+    print("My name is: ${name}");
+    print("My favorite app is: ${favoriteApp}");
+    print("My city is: ${city}");
+  
+}
